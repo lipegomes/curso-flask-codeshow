dab4d60fed4ecbae0b7e48e4a9f3d3f98bf9c519
lipegomes <fgdl.py91@gmail.com>
2021-03-13 13:17:28 -0300

    Codecov github actions

--- delivery/ext/site/main.py
+++ delivery/ext/site/main.py
@@ -4,9 +4,7 @@ from flask import Blueprint
 
 bp = Blueprint("site", __name__)
 
+
 @bp.route("/")
 def index():
-    return render_template(
-        "index.html",
-        # name=request.args["name"]
-    )
+    return render_template("index.html")
