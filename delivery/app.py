43e57fbb9073084ab8b291cc084d256c1f1a7c51
lipegomes <fgdl.py91@gmail.com>
2021-03-14 16:57:13 -0300

    Update templates and macro

--- delivery/app.py
+++ delivery/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from delivery.ext import site
+from delivery.ext import config
+from delivery.ext import toolbar
 
 
 # Enter point
 def create_app():
     """Factory principal"""
     app = Flask(__name__)
+    config.init_app(app)
+    toolbar.init_app(app)
     site.init_app(app)
 
     return app
